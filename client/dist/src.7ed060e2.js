9cee9b54a6eca665f5d07fb18e6ede0824cb3069
Shafee <shafeelinks@gmail.com>
2021-10-21 15:36:36 -0400

    test branch

--- client/dist/src.7ed060e2.js
+++ client/dist/src.7ed060e2.js
@@ -29698,7 +29698,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58916" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54593" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div>
       <h1>Hello from App.js</h1>
+      <h1>Hey Nicholas, from new branch!</h1>
     </div>
   );
 }
