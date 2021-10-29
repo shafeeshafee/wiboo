3456e7ad8e9e78424116d32577c4ee606f800b4c
Shafee <shafeelinks@gmail.com>
2021-10-29 11:33:33 -0400

    comments added in auth page

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,12 +4,12 @@ const { check, validationResult } = require("express-validator");
 const JWT = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { User } = require("../models/User");
-const users = require("../db");
 
 // SIGNUP
 router.post(
   "/signup",
   [
+    // email + password validation with express-validator
     check("email", "Please input a valid email.").isEmail(),
     check(
       "password",
@@ -22,22 +22,23 @@ router.post(
     // Validate the inputs
     const errors = validationResult(req);
 
+    // if errors exist in our validation, respond back with a JSON of all the errors
     if (!errors.isEmpty()) {
       return res.status(422).json({
         errors: errors.array(),
       });
     }
 
+    // Sequelize method to find all users "where" the data matches
     let checkUser = await User.findAll({
       where: {
         username: email,
       },
     });
 
-    let userAlreadyExists = Array.isArray(checkUser) && checkUser.length;
-
     // if theres already a user, then its value will be more than 0, i.e. 1
     // so if there is, return error
+    let userAlreadyExists = Array.isArray(checkUser) && checkUser.length;
     if (userAlreadyExists > 0) {
       return res.status(422).json({
         errors: [
@@ -48,19 +49,21 @@ router.post(
       });
     }
 
-    // Hash the password
+    // Hash the password using bcrypt
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Save the password into the db
+    // Save the email and password into the db
     const postUser = await User.create({
       username: email,
       password: hashedPassword,
     });
 
+    // Assign a JWT token upon signup using their email
     const token = await JWT.sign({ email }, process.env.ACCESS_SECRET, {
       expiresIn: 360000,
     });
 
+    // Send JWT signup token
     return res.status(201).json({
       token,
       postUser,
@@ -79,8 +82,9 @@ router.post("/login", async (req, res) => {
     },
   });
 
+  // if db has this user, then its value will be more than 0, i.e. 1
   let dbHasUser = Array.isArray(user) && user.length;
-
+  // if db doesn't have this user, we respond "invalid credentials" error
   if (!dbHasUser) {
     return res.status(422).json({
       errors: [
@@ -91,7 +95,7 @@ router.post("/login", async (req, res) => {
     });
   }
 
-  // Check if the password if valid
+  // Check if the password they entered is valid using bcrypt
   let isMatch = await bcrypt.compare(password, user[0].password);
 
   if (!isMatch) {
@@ -104,17 +108,12 @@ router.post("/login", async (req, res) => {
     });
   }
 
-  // Send JSON WEB TOKEN
+  // Sign their login with a JWT token
   const token = await JWT.sign({ email }, process.env.ACCESS_SECRET, {
     expiresIn: 360000,
   });
 
-  let getUser = await User.findAll({
-    where: {
-      username: email,
-    },
-  });
-
+  // Send JWT
   res.json({
     token,
   });
